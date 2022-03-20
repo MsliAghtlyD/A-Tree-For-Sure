18d9d5ec7973ce172a8549a27d4da3066ebec142
MsliAghtlyD <redarthurx@gmail.com>
2022-03-20 13:04:00 +0100

    Update mod.js
    I am just getting starting.
    Or just about to abandon this

--- js/mod.js
+++ js/mod.js
@@ -1,7 +1,7 @@
 let modInfo = {
-	name: "The ??? Tree",
-	id: "mymod",
-	author: "nobody",
+	name: "A Tree For Sure",
+	id: "thismodsurewaswrittenbymsliaghtlyd",
+	author: "MsliAghtlyD",
 	pointsName: "points",
 	modFiles: ["layers.js", "tree.js"],
 
