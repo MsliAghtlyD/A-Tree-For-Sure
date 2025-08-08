e6058e28ced14597f806917b3832de9902f2c7e0
MsliAghtlyD <redarthurx@gmail.com>
2025-08-08 10:32:02 +0200

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -159,7 +159,8 @@ let changelog = `<h1>Changelog:</h1><br><br>
 		- Created a lot more<br><br>
 	<h3>v0.04999a</h3><br>
 		- Added a new locked tooltip for the friends and forum layers so people would be sure it was not just a bug<br>
-		- Theory upgrades are now spliced correctly again
+		- Theory upgrades are now spliced correctly again<br>
+		- Added a cap to clue upgrades and theory layer effect to prevent infinite exponention
 		`
 	
 
